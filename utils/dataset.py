0812a798eb2f348aa85c31294a3e96527c15dad6
codecat0 <2049413359@qq.com>
2021-06-19 23:00:08 +0800

    update dataset file

--- utils/dataset.py
+++ utils/dataset.py
@@ -5,27 +5,23 @@
 """
 import glob
 import hashlib
-import logging
-import math
 import os
-import random
-import shutil
-import time
 from itertools import repeat
 from multiprocessing.pool import ThreadPool
 from pathlib import Path
-from threading import Thread
 
 import cv2
 import numpy as np
 import torch
-import torch.nn.functional as F
 from PIL import Image, ExifTags
 from torch.utils.data import Dataset
 from tqdm import tqdm
 
+from utils.general import xyxy2xywh, xywhn2xyxy
+
 help_url = 'https://github.com/ultralytics/yolov3/wiki/Train-Custom-Data'
 img_formats = ['bmp', 'jpg', 'jpeg', 'png', 'tif', 'tiff', 'dng', 'webp', 'mpo']
+
 
 # 获取一组文件路径列表的hash值
 def get_hash(paths):
@@ -62,17 +58,14 @@ class LoadImagesAndLabels(Dataset):   # for training/testing
                  path,  # 指向data/images的路径
                  img_size=416,   # 预处理后输出的图像尺寸
                  batch_size=16,
-                 augment=False,  # 图像增强 （颜色空间）
                  hyp=None,  # 超参数字典
                  rect=False,    # 是否使用rectangular training
                  cache_images=False,    # 是否缓存图片到内存
                  stride=32,    # 将图片尺寸调整到stride的整数倍
                  pad=0.0):
         self.img_size = img_size
-        self.augment = augment
         self.hyp = hyp
         self.rect = rect
-        self.mosaic = self.augment and not self.rect    # 将4张图片拼接在一张马赛克图像中
         self.path = path
 
         # 定义images
@@ -105,8 +98,81 @@ class LoadImagesAndLabels(Dataset):   # for training/testing
         else:
             cache, exists = self.cache_labels(cache_path), False
 
-        # 读取cache
+        # 查看缓存信息
+        nf, nm, ne, nc, n = cache.pop('results')
+        if exists:
+            d = f"Scanning '{cache_path}' images and labels... {nf} found, {nm} missing, {ne} empty, {nc} corrupted"
+            tqdm(None, desc=d, total=n, initial=n)
 
+        # 读取缓存信息
+        # cache = {'im_file1': [label, shape], 'im_file2': [label, shape], ..., 'hash': hash_value, 'results': (nf, nm, ne, nc, n)}
+        cache.pop('hash')   # 移除hash
+        labels, shapes = zip(*cache.values())
+        self.labels = list(labels)
+        self.shapes = np.array(shapes, dtype=np.float64)
+        # 更新图像文件信息
+        self.img_files = list(cache.keys())
+        # 更新图像标签信息
+        self.label_files = [x.replace('images', 'labels').replace(os.path.splitext(x)[-1], '.txt')
+                            for x in self.img_files]
+
+        # 图像的数量
+        n = len(shapes)
+        # batch索引
+        bi = np.floor(np.arange(n) / batch_size).astype(np.int)
+        # batch的数量
+        nb = bi[-1] + 1
+        self.batch = bi
+        self.n = n
+        self.indices = range(n)
+
+        # 是否使用类似原图像比例的矩形进行训练，即最长边为img_size，而不是img_size x img_size
+        if self.rect:
+            s = self.shapes
+            # 获取每个图像的高/宽比
+            ar = s[:, 1] / s[:, 0]
+            # 按照高宽比进行排序，使得每个batch中的数据具有类似的高宽比
+            irect = ar.argsort()
+            # 根据排序后的结果重新设置图像顺序，标签顺序和图像shape数据
+            self.img_files = [self.img_files[i] for i in irect]
+            self.label_files = [self.label_files[i] for i in irect]
+            self.labels = [self.labels[i] for i in irect]
+            self.shapes = s[irect]
+            ar = ar[irect]
+
+            # 计算每一个batch中的数据的图像尺寸
+            shapes = [[1, 1]] * nb
+            for i in range(nb):
+                ari = ar[bi == i]
+                # 获取第i个batch中的最大高宽比和最小高宽比
+                mini, maxi = ari.min(), ari.max()
+                # 如果最大的高宽比<1, 说明h<w，将w设置为img_size
+                if maxi < 1:
+                    shapes[i] = [maxi, 1]
+                # 如果最小高宽比>1，说明h>w，将h设置为img_size
+                elif mini > 1:
+                    shapes[i] = [1, 1 / mini]
+
+            # 计算每个batch输入网络的shape值(向上取整为stride(32)的整数倍)
+            self.batch_shapes = np.ceil(np.array(shapes) * img_size / stride + pad).astype(np.int) * stride
+
+        # Cache images into memory for faster training (WARNING: large datasets may exceed system RAM)
+        self.imgs = [None] * n
+        if cache_images:
+            # 用于记录缓存图像占用RAM大小
+            gb = 0
+            # 原始图像尺寸，resize后的图像尺寸
+            self.img_hw0, self.img_hw = [None] * n, [None] * n
+            # for i in range(n):
+            #       load_image(self, i)
+            # 采用8线程
+            results = ThreadPool(8).imap(lambda x: load_image(*x), zip(repeat(self), range(n)))
+            pbar = tqdm(enumerate(results), total=n)
+            for i, x in pbar:
+                self.imgs[i], self.img_hw0[i], self.img_hw[i] = x
+                gb += self.imgs[i].nbytes
+                pbar.desc = f'Caching images ({gb / 1E9:.1f}GB)'
+            pbar.close()
 
     # 将数据集的labels信息加载到缓存，检查images信息和读取image形状
     def cache_labels(self, path=Path('./labels.cache')):
@@ -162,3 +228,113 @@ class LoadImagesAndLabels(Dataset):   # for training/testing
             print(f'WARNING: Cache directory {path.parent} is not writeable: {e}')
 
         return x
+
+    def __len__(self):
+        return len(self.img_files)
+
+    def __getitem__(self, index):
+        hyp = self.hyp
+        # 导入图像
+        img, (h0, w0), (h, w) = load_image(self, index)
+
+        shape = self.batch_shapes[self.batch[index] if self.rect else self.img_size]
+        img, ratio, pad = letterbox(img, shape, auto=False)
+        shapes = (h0, w0), ((h / h0, w / w0), pad)  # for COCO mAP resacling
+
+        labels = self.labels[index].copy()
+        if labels.size:
+            labels[:, 1:] = xywhn2xyxy(labels[:, 1:], ratio[0] * w, ratio[1] * h, padw=pad[0], padh=pad[1])
+
+        nL = len(labels)    # 标签的数量
+        if nL:
+            labels[:, 1:5] = xyxy2xywh(labels[:, 1:5])
+            labels[:, [2, 4]] /= img.shape[0]   # 将高度标准化 0-1
+            labels[:, [1, 3]] /= img.shape[1]   # 将宽度标准化 0-1
+
+        # (batch中图像索引，框中物体类别，x, y, w, h)
+        labels_out = torch.zeros((nL, 6))
+        if nL:
+            labels_out[:, 1:] = torch.from_numpy(labels)
+
+        # Convert BGR to RGB and HWC to CHW
+        img = img[:, :, ::-1].transpose(2, 0, 1)
+        img = np.ascontiguousarray(img)
+
+        return torch.from_numpy(img), labels_out, self.img_files[index], shapes
+
+    @staticmethod
+    def collate_fn(batch):
+        img, label, path, shapes = zip(*batch)
+        for i, l in enumerate(label):
+            # 添加batch中图像索引
+            l[:, 0] = i
+        return torch.stack(img, 0), torch.cat(label, 0), path, shapes
+
+
+def load_image(self, index):
+    """
+    从数据集中加载一张图像，返回图像，原始图像尺寸，resize后的尺寸
+    """
+    img = self.imgs[index]
+    # 没有缓存图像信息
+    if img is None:
+        path = self.img_files[index]
+        # BGR
+        img = cv2.imread(path)
+        assert img is not None, 'Image Not Found' + path
+        # 原始图像尺寸
+        h0, w0 = img.shape[:2]
+        # img_size 设置的是预处理后输出的图像尺寸
+        r = self.img_size / max(h0, w0)
+        if r != 1:
+            img = cv2.resize(img, (int(w0 * r), int(h0 * r)),
+                             interpolation=cv2.INTER_AREA if r < 1 and not self.augment else cv2.INTER_LINEAR)
+        return img, (h0, w0), img.shape[:2]
+    else:
+        return self.imgs[index], self.img_hw0[index], self.img_hw[index]
+
+
+def letterbox(img,
+              new_shape=(416, 416),
+              color=(114, 114, 114),
+              auto=True,
+              scaleFill=False,
+              scaleup=True,
+              stride=32):
+    """将图像调整到指定大小"""
+
+    shape = img.shape[:2]
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
+
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+
+    # 对于大于指定输入大小的图像进行调整，小于的不变（for better test mAP）
+    if not scaleup:
+        r = min(r, 1.0)
+
+
+    # 计算padding
+    ratio = r, r
+    new_unpad = int(round(shape[0] * r)), int(round(shape[1] * r))
+    dh, dw = new_shape[0] - new_unpad[0], new_shape[1] - new_unpad[1]   # wh padding
+    # 保证原图比例不变
+    if auto:
+        dw, dh = np.mod(dw, stride), np.mod(dh, stride)
+    # 直接将图像缩放到指定尺寸
+    elif scaleFill:
+        dw, dh = 0.0, 0.0
+        new_unpad = (new_shape[1], new_shape[0])
+        ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]
+
+    # 将padding分到左右，上下两侧
+    dw /= 2
+    dh /= 2
+
+    if shape != new_unpad:
+        img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
+
+    return img, ratio, (dw, dh)
--- utils/general.py
+++ utils/general.py
@@ -4,7 +4,27 @@
 @Time : 2021/6/18 下午5:37
 """
 import math
+import torch
+import numpy as np
 
 
 def make_divisible(x, divisor):
     return math.ceil(x / divisor) * divisor
+
+
+def xywhn2xyxy(x, w=640, h=640, padw=0, padh=0):
+    y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
+    y[:, 0] = w * (x[:, 0] - x[:, 2] / 2) + padw    # xmin
+    y[:, 1] = h * (x[:, 1] - x[:, 3] / 2) + padh    # ymin
+    y[:, 2] = w * (x[:, 2] + x[:, 2] / 2) + padw    # xmax
+    y[:, 3] = h * (x[:, 3] + x[:, 3] / 2) + padh    # ymax
+    return y
+
+
+def xyxy2xywh(x):
+    y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
+    y[:, 0] = (x[:, 0] + x[:, 2]) / 2   # center x
+    y[:, 1] = (x[:, 1] + x[:, 3]) / 2   # center y
+    y[:, 2] = x[:, 2] - x[:, 0]     # width
+    y[:, 3] = x[:, 3] - x[:, 1]     # height
+    return y
