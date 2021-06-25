4cbe6b2c45d4b9901e23534d771e0fe156a9fe99
codecat <2049413359@qq.com>
2021-06-25 10:47:10 +0800

    modify utils files

--- models/yolo.py
+++ models/yolo.py
@@ -46,13 +46,13 @@ class Detect(nn.Module):
                 if self.grid[i].shape[2:4] != x[i].shape[2:4]:
                     self.grid[i] = self._make_grid(nx, ny).to(x[i].device)
 
-                # y = x[i].sigmoid()
-                # y[..., 0:2] = (y[..., 0:2] * 2. - 0.5 + self.grid[i]) * self.stride[i]  # xy
-                # y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
-                y = x[i].clone()
-                y[..., 0:2] = (torch.sigmoid(y[..., 0:2]) + self.grid[i]) * self.stride[i]  # xy
-                y[..., 2:4] = torch.exp(y[...:2:4]) * self.anchor_grid[i]   # wh
-                torch.sigmoid(y[..., 4:])
+                y = x[i].sigmoid()
+                y[..., 0:2] = (y[..., 0:2] * 2. - 0.5 + self.grid[i]) * self.stride[i]  # xy
+                y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
+                # y = x[i].clone()
+                # y[..., 0:2] = (torch.sigmoid(y[..., 0:2]) + self.grid[i]) * self.stride[i]  # xy
+                # y[..., 2:4] = torch.exp(y[...:2:4]) * self.anchor_grid[i]   # wh
+                # torch.sigmoid(y[..., 4:])
                 z.append(y.view(bs, -1, self.no))
         return x if self.training else (torch.cat(z, 1), x)
 
--- utils/general.py
+++ utils/general.py
@@ -22,8 +22,8 @@ def xywhn2xyxy(x, w=640, h=640, padw=0, padh=0):
     y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
     y[:, 0] = w * (x[:, 0] - x[:, 2] / 2) + padw    # xmin
     y[:, 1] = h * (x[:, 1] - x[:, 3] / 2) + padh    # ymin
-    y[:, 2] = w * (x[:, 2] + x[:, 2] / 2) + padw    # xmax
-    y[:, 3] = h * (x[:, 3] + x[:, 3] / 2) + padh    # ymax
+    y[:, 2] = w * (x[:, 0] + x[:, 2] / 2) + padw    # xmax
+    y[:, 3] = h * (x[:, 1] + x[:, 3] / 2) + padh    # ymax
     return y
 
 
